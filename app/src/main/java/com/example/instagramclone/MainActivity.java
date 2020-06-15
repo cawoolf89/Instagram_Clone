2241f7df59926936b1917753adf464b9d497522a
Cam <cawoolf89@gmail.com>
2020-06-15 12:18:11 +0000

    Clean start working branch.

--- app/src/main/java/com/example/instagramclone/MainActivity.java
+++ app/src/main/java/com/example/instagramclone/MainActivity.java
@@ -1,9 +1,13 @@
+//You are still in the working branch.
+
+
 package com.example.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +16,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.i("Msg", "Trail push");
-        Log.i("Msg", "Trail push");
-        Log.i("Msg", "This is the master branch");
-        Log.i("Msg", "This is the merge from the master into the working branch");
-        //You are still in the working branch.
     }
 }
