4914de7fe2bd43ce31d734370a4532534bf184ab
Cam <cawoolf89@gmail.com>
2020-06-15 11:55:41 +0000

    Trail commit, Log.i in main activity take 2

--- app/src/main/java/com/example/instagramclone/MainActivity.java
+++ app/src/main/java/com/example/instagramclone/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.i("Msg", "Trail push");
+        Log.i("Msg", "Trail push");
     }
 }
